5bf9bd8c57605cf13b2aa014a37fcb9d59ee1f5c
Arshan Khanifar <arshankhanifar@Arshans-MacBook-Pro-2.local>
2021-12-29 20:34:16 -0500

    added web3 starter

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,20 @@
+import { ethers } from "ethers";
+import { config } from "dotenv";
+
 const main = async () => {
-  console.log('hello, world!');
-}
+  config();
+  const { PROVIDER, MY_ADDRESS } = process.env;
+  if (!PROVIDER) {
+    throw new Error("No provider");
+  }
+  if (!MY_ADDRESS) {
+    throw new Error("No address");
+  }
+  const provider = ethers.getDefaultProvider(PROVIDER);
+  const balanceWei = await provider.getBalance(MY_ADDRESS);
+  const balance = ethers.utils.formatEther(balanceWei);
+  console.log(`hello, world! Your eth balance is: ${balance}`);
+};
 
 main().catch((error) => {
   console.error(error);
